78b53c48420ded1066aa33e137fa114172dbae1c
Scalaptia <fernando@haroware.com>
2022-04-08 17:34:01 -0700

    Add DOM elements and attributes.

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+let displayValue = ""
+
+const numberButtons = document.querySelectorAll('[number-btn]')
+const operatorButtons = document.querySelectorAll('[operator-btn]')
+const equalsButton = document.getElementById('equals-btn')
+const pointButton = document.getElementById('point-btn')
+const clearButton = document.getElementById('clear-btn')
+const deleteButton = document.getElementById('delete-btn')
+const lastScreen = document.getElementById('lastScreen')
+const currentScreen = document.getElementById('currentScreen')
+  
   const add = function(a, b) {
       return a + b;
   };
